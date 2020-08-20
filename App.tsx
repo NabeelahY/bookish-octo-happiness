771a82bd6fe7e7e498c7e940940a333692600479
NabeelahY <noblenab13@gmail.com>
2020-08-20 17:48:51 +0100

    Add sign up functionality via aws amplify

--- App.tsx
+++ App.tsx
@@ -9,34 +9,57 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Amplify, {Auth} from 'aws-amplify';
+import awsConfig from './config/aws-exports';
+
+Amplify.configure(awsConfig);
 
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
+  const userSignUp = () => {
+    Auth.signUp({
+      username: 'user',
+      password: 'nab123456#',
+      attributes: {
+        email: 'noblenab13@gmail.com',
+      },
+    })
+      .then((res) => {
+        console.log('signed in', res);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  };
+
+  const confirm = () => {
+    Auth.confirmSignUp('user', '835695')
+      .then((res) => {
+        console.log('signed in', res);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  };
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>Hello there!</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <Text>Hello there</Text>
+      <Button title="Sign Up" onPress={userSignUp} />
+      <Button title="Confirm" onPress={confirm} />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
