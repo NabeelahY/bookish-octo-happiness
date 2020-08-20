db8ce7890f110a06c011ff4a4d68d9cfc8cdd121
NabeelahY <noblenab13@gmail.com>
2020-08-20 17:47:16 +0100

    Declare types for .env file variables

--- config/aws-exports.ts
+++ config/aws-exports.ts
@@ -1,8 +1,15 @@
+import {
+  REACT_APP_IDENTITY_POOL_ID,
+  REACT_APP_REGION,
+  REACT_APP_USER_POOL_ID,
+  REACT_APP_USER_POOL_WEB_CLIENT_ID,
+} from 'react-native-dotenv';
+
 const awsConfig = {
-  identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
-  region: process.env.REACT_APP_REGION,
-  userPoolId: process.env.REACT_APP_USER_POOL_ID,
-  userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+  identityPoolId: REACT_APP_IDENTITY_POOL_ID,
+  region: REACT_APP_REGION,
+  userPoolId: REACT_APP_USER_POOL_ID,
+  userPoolWebClientId: REACT_APP_USER_POOL_WEB_CLIENT_ID,
 };
 
 export default awsConfig;
---
+++ global.d.ts
@@ -0,0 +1,7 @@
+declare module 'aws-amplify-react-native';
+declare module 'react-native-dotenv' {
+  export const REACT_APP_IDENTITY_POOL_ID: string;
+  export const REACT_APP_REGION: string;
+  export const REACT_APP_USER_POOL_ID: string;
+  export const REACT_APP_USER_POOL_WEB_CLIENT_ID: string;
+}
